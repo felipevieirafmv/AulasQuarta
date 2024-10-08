1563d4ca73e890d1e25e2fc08967b9e8abb523f6
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-10-08 13:24:41 -0300

    :sparkles: Adding routes in app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var User = require('./models/user')(sequelize);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/product');
 
 var app = express();
 
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 const db = require('./models')
 
